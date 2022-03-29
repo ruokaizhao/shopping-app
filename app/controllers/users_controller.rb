254b8a36e096b3ff1948f5321e78babc91eaa95b
Ruokai <lelouch_l@icloud.com>
2022-03-29 04:16:49 -0400

    Add User model, users and sessions controller as well as routes and controller actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :authorize, only: [:show]
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  def show
+    user = User.find(session[:user_id])
+    render json: user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, password_confirmation, :name, :email)
+  end
+
+  def render_unprocessable_entity_response
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
+  def authorize
+    return render json: { errors: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
+
+end
