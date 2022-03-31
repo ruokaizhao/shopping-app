20d079b6555e6654ea2b0711fef2f855f6f73db9
Ruokai <lelouch_l@icloud.com>
2022-03-31 10:39:09 -0400

    Change useEffect hook for fetching cart data from Carts.js to NavBar.js so that state is loaded automatically after refreshing the page

--- client/src/components/Carts.js
+++ client/src/components/Carts.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchCarts } from './cartSlice';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import ProductInCart from "./ProductInCart";
 
 function Cart() {
   const carts = useSelector((state) => state.carts.entities)
   const total = carts.reduce((previous, current) => previous + current.price * current.quantity, 0)
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(fetchCarts())
-  }, [])
 
 
   return (
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCarts } from './cartSlice';
 
 function NavBar() {
   const carts = useSelector((state) => state.carts.entities)
   const itemsInCarts = carts.reduce((previous, current) => previous + current.quantity, 0)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchCarts())
+  }, [])
 
   return (
     <div>
