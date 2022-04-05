88577fe213c1b400eba43951552b3d70ae20bc6d
Ruokai <lelouch_l@icloud.com>
2022-04-05 07:21:04 -0400

    Add some comments

--- app/serializers/product_review_serializer.rb
+++ app/serializers/product_review_serializer.rb
@@ -1,3 +1,4 @@
+# Return reviews with corresponding product so that frontend can render related reviews for a product.
 class ProductReviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :image, :description
   has_many :reviews, serializer: UserReviewSerializer
--- app/serializers/user_review_serializer.rb
+++ app/serializers/user_review_serializer.rb
@@ -1,3 +1,5 @@
+# To return custome "name" property with reviews (reviews that returned with corresponding product) so that
+# frontend can access user name when render reviews.
 class UserReviewSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :product_id, :content, :rating, :name
 
