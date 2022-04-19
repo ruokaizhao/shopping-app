7ba2ac5943f3868831adb1dba5ccf80807961625
Ruokai <lelouch_l@icloud.com>
2022-04-19 02:28:41 -0400

    Fix minor bugs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
-  validates :content, presence: true, length: { maximum: 10 }
+  validates :content, presence: true, length: { minimum: 10 }
   validates :rating, presence: true
 end
