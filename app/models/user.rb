6db38158bd808b7211bde7e562bbe17c2ac7a84e
Ruokai <lelouch_l@icloud.com>
2022-05-02 03:10:57 -0400

    Create send_password_reset, reset_password and generate_base64_token methods in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,22 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true, length: {maximum: 254}
-  validates :password, presence: true, length: {minimum: 6}
+  validates :password, presence: true, length: {minimum: 6}, if: :password
+
+  def send_password_reset
+    self.update!(password_reset_token: self.generate_base64_token, password_reset_sent_at: Time.zone.now)
+    UserMailer.with(user: self, token: self.password_reset_token).password_reset_email.deliver_now
+  end
+
+  def reset_password(params)
+      self.update!(password: params[:password], password_confirmation: params[:password_confirmation])      
+      self.update!(password_reset_token: nil, password_reset_sent_at: nil)    
+  end
+
+  private
+
+  def generate_base64_token
+    SecureRandom.urlsafe_base64
+  end
+
 end
