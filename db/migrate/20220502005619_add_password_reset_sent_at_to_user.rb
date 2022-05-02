80be66fe275b68e5de5388032a19581c2e533f50
Ruokai <lelouch_l@icloud.com>
2022-05-02 02:57:22 -0400

    Create new migration for resetting password

---
+++ db/migrate/20220502005619_add_password_reset_sent_at_to_user.rb
@@ -0,0 +1,5 @@
+class AddPasswordResetSentAtToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :password_reset_sent_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_08_162724) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_005619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_08_162724) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
