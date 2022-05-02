a4e9c41a63aecc27fbc7d7094e281d0846b48da9
Ruokai <lelouch_l@icloud.com>
2022-05-02 03:07:52 -0400

    Create two methods in UserMailer class for welcome_email and password_reset_email

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,17 @@
+class UserMailer < ApplicationMailer
+  default from: 'zhaozrk@gmail.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'zhaozrk@gmail.com'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+  def password_reset_email
+    @user = params[:user]
+    @token = params[:token]
+    @url  = "http://localhost:4000/reset_password/#{@token}"
+    mail(to: @user.email, subject: 'Password reset instructions')
+  end  
+
+end
